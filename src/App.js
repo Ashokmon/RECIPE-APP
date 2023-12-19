d1b9553df0078df0b3111458d39df5d8f1a1cef4
Ashokmon <ashokmon07@gmail.com>
2023-12-19 12:17:14 +0530

    recipe app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { fetchRecipes, fetchRecipesById } from "./utils/api";
+import Loader from "./components/loader/loader";
+import Header from "./components/Header/Header"
+import RecipeList from "./components/RecipeList/RecipeList";
+
 
 function App() {
+
+  //reload,rerender --- state data change
+  const [recipes, setRecipes] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [searchQuery, setSearchQuery] = useState("")
+
+
+
+  useEffect(() => {
+    const fetchRecipesData = async () => {
+      try {
+        const data = await fetchRecipes()
+        setRecipes(data)
+        setLoading(!true)
+      }
+      catch {
+        setLoading(false)
+      }
+    }
+    fetchRecipesData()
+  }, [])
+
+
+  const filterRecipe = recipes.filter(rec => rec.title.toLowerCase().includes(searchQuery.toLowerCase()))
+
+  const recipesToDisplay = searchQuery ? filterRecipe : recipes
+  // fetchRecipes()
+  // fetchRecipesById()
+
+  // fetchRecipe(getAllRecipesURL)
+  // fetchRecipe(getRecipesURLByID)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title={"Recipe App"} setSearchQuery={setSearchQuery} />
+
+      {loading ? <Loader name={"Recipes is Loading"} /> :
+        <RecipeList recipes={recipesToDisplay} />}
     </div>
   );
 }
 
-export default App;
+export default App
